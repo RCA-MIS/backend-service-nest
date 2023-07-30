773f33140862b46fc91f224f01ba27372a6a3c7a
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-07-30 02:59:44 +0200

    [Add] : Added the user , student , teacher modules , controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { StudentsModule } from './students/students.module';
+import { TeachersModule } from './teachers/teachers.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, StudentsModule, TeachersModule]
 })
 export class AppModule {}
