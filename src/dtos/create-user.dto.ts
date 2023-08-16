262f27afccf4d8d3bc3966f531573ee5b1518739
valens200 <valensniyonsenga2003@gmail.com>
2023-08-16 04:10:59 +0200

    [feat]: added operations to dtos

--- src/dtos/create-user.dto.ts
+++ src/dtos/create-user.dto.ts
@@ -1,36 +1,44 @@
 /* eslint-disable */ 
-import { IsString , IsNotEmpty, IsEnum } from "class-validator";
+import { IsString , IsNotEmpty, IsEnum, IsEmail, IsStrongPassword } from "class-validator";
 import { EGender } from "../Enum/EGender.enum";
 import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateUserDto{
     @IsString()
     @IsNotEmpty()
+    @IsEmail()
     @ApiProperty()
     email : string;
     
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     username : string;
 
     @IsString()
     @IsNotEmpty()
     @IsEnum(EGender)
+    @ApiProperty()
     myGender : EGender;
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     registercode : string;
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     national_id : string;
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     phonenumber : string;
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
+    @IsStrongPassword()
     password : string;
 }
