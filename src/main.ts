3f13d6fc46ee8c6af19c818c6eeb4f01af5c9621
valens200 <valensniyonsenga2003@gmail.com>
2023-09-10 22:06:25 +0200

    [fix]: fixed cors

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cors({
+      origin: '*',
+    }),
+  );
   const config = new DocumentBuilder()
     .setTitle('RCA-mis BACKEND API')
     .setDescription(
