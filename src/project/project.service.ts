f9c53ffe9f0277d700cfe8d90787023e597f5aec
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-09-14 23:20:51 +0200

    Made the file upload of the project optional

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -34,7 +34,12 @@ export class ProjectService {
         if(!user) return new NotFoundException(`User with email: ${userEmail} not found`);
         const createdAt = new Date(Date.now())
         const updatedAt = null;
-        const image = await this.fileService.uploadFile(file);
+        let image;
+        if(!file || file == null || file == undefined) {
+            image = null;
+        }else{
+            image = await this.fileService.uploadFile(file);
+        }
         let newstatus = status.toString();
         const projectEntity = this.projectRepo.create({
             name,
@@ -59,8 +64,13 @@ export class ProjectService {
             }
         });
         if(project){
-            const image = await this.fileService.uploadFile(file);
-            project.image = image;
+            let newImage;
+            if(project.image != null || project.image != undefined || project.image != ""){
+                newImage =this.fileService.updateFile(project.image , file);
+            }else{
+                newImage = await this.fileService.uploadFile(file);
+            }
+            project.image = newImage;
             await this.projectRepo.save(project);
             return {
                 message : "Project image updated successfully"
@@ -96,6 +106,8 @@ export class ProjectService {
         if(!project){
             return new NotFoundException("Project not found");
         }
+
+        await this.fileService.deleteFile(project.image);
 
         this.projectRepo.remove(project);
         return {
