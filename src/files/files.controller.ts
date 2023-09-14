bed7e05e4023d29a43ced93293515f1683e770fd
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-09-14 20:41:28 +0200

    Added other endpoints in the file controller

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import { BadRequestException, Controller, Get, NotFoundException, Param, Post, UseInterceptors , UploadedFile, Delete, Patch } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { FilesService } from './files.service';
 import { Res } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('files')
 @ApiTags('files')
@@ -19,4 +20,46 @@ export class FilesController {
 
       return res.sendFile(filename, {root: 'uploads'})
     }
+
+    @Post('/upload')
+    @UseInterceptors(FileInterceptor('file'))
+    async uploadFile ( @UploadedFile() file: Express.Multer.File) {
+       const fileName = await this.fileService.uploadFile(file);
+       if(!fileName || fileName == null){
+        return new BadRequestException('File not uploaded try again with the file tag' );
+    }
+    return {
+        message : 'File uploaded successfully',
+        filename : fileName
+    }
+}
+
+@Delete('/delete/:filename')
+async deleteFile(@Param('filename') filename){
+    const fileLocation = this.fileService.getFile(filename);
+    if(!fileLocation || fileLocation == null){
+        return new NotFoundException('File not found');
+    }
+    await this.fileService.deleteFile(filename);
+    return {
+        message : 'File deleted successfully'
+    }
+}
+
+@Patch('/update/:filename')
+@UseInterceptors(FileInterceptor('file'))
+async updateFile(@Param('filename') filename, @Res() res,  @UploadedFile() file: Express.Multer.File){
+    const fileLocation = this.fileService.getFile(filename);
+    if(!fileLocation || fileLocation == null){
+        return new NotFoundException('File not found');
+    }
+    const fileName = await this.fileService.updateFile(filename, file);
+    if(!fileName || fileName == null){
+        return new BadRequestException('File not uploaded try again with the file tag' );
+    }
+    return {
+        message : 'File uploaded successfully',
+        filename : fileName
+    }
+}
 }
